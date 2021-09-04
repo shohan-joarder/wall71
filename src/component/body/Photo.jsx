607289ad7d6c659579a1cfd236e3256f3a092829
shohan-joarder <jshohan887@gmail.com>
2021-09-04 14:20:18 +0600

    image convert to wenp

--- src/component/body/Photo.jsx
+++ src/component/body/Photo.jsx
@@ -14,7 +14,7 @@ export default function Photo({ id, collections_name, name, photo }) {
     <Grid item xs={6} sm={3} lg={2}>
       <Box className={classes.imageContainer}>
         <img
-          src={`${photo_url}/${photo}`}
+          src={`${photo_url}/${photo}.webp`}
           alt={name}
           className={classes.responsiveImg}
         />
--- src/component/common/Url.jsx
+++ src/component/common/Url.jsx
@@ -1,3 +1,3 @@
 export const url = "https://wall71.coder71.com/api";
-export const photo_url = `https://wall71.coder71.com/images/products`;
+export const photo_url = `https://wall71.coder71.com/images/products/webp`;
 export const collection_photo = `https://wall71.coder71.com/images/collections`
